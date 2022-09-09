0d3f5025c306fb99b055601754a21fe093e72269
Arturo <arturo@tryp.mx>
2022-09-09 13:32:02 -0500

    enviromental test

--- env.js
+++ env.js
@@ -1,11 +1,11 @@
-// import dotenv from "../dotenv";
+// import dotenv from "./dotenv";
 
 // dotenv.config();
 // const token2 = process.env.AIRTABLE_API_TOKEN;
+
+// console.log(token2);
 
 // export function getAirtableToken() {
 //   console.log(token2);
 //   return token2;
 // }
-
-// getAirtableToken();
