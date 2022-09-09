23bc0b7df555c349b175590d05b1376290c213da
Arturo <arturo@tryp.mx>
2022-09-08 19:45:09 -0500

    Actualizado

--- env.js
+++ env.js
@@ -1,2 +1,11 @@
-import dotenv from "dotenv";
-dotenv.config({ silent: true });
+import dotenv from "../dotenv";
+
+dotenv.config();
+const token2 = process.env.AIRTABLE_API_TOKEN;
+
+export function getAirtableToken() {
+  console.log(token2);
+  return token2;
+}
+
+getAirtableToken();
